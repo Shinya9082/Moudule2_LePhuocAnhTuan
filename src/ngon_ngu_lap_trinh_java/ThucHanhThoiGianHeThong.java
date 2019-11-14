bdea609ff24412a0120848544c03c6c0c86dc699
Shinya9082 <anhtuandtu1993@gmail.com>
2019-11-14 14:38:25 +0700

    Thuc hanh hien thi thoi gian

---
+++ src/ngon_ngu_lap_trinh_java/ThucHanhThoiGianHeThong.java
@@ -0,0 +1,10 @@
+package ngon_ngu_lap_trinh_java;
+
+import java.util.Date;
+
+public class ThucHanhThoiGianHeThong {
+    public static void main(String[] args) {
+        Date now= new Date();
+        System.out.println("Now is: "+now);
+    }
+}
